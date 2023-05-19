2474dafc78129caa257c902b71051fadaa406fff
Seokhyun Lee <lseokhyun99@gmail.com>
2023-05-19 11:06:31 +0900

    Professor 검색 기능 추가 - 코드 기반 검색 - 이름 기반 검색 ( 테스트 용 )

---
+++ src/main/java/chat/chatbot/repository/ProfessorRepository.java
@@ -0,0 +1,18 @@
+package chat.chatbot.repository;
+
+import chat.chatbot.menu.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+    @Query("SELECT p FROM Professor p WHERE p.name = :name")
+    Professor findByName(@Param("name") String name);
+
+    @Query("SELECT p FROM Professor p WHERE p.code = :code")
+    Professor findByCode(@Param("code") String code);
+
+}
---
+++ src/main/java/chat/chatbot/service/ProfessorService.java
@@ -0,0 +1,25 @@
+package chat.chatbot.service;
+
+import chat.chatbot.menu.Professor;
+import chat.chatbot.repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessorService {
+
+    private static ProfessorRepository professorRepository;
+
+    @Autowired
+    public ProfessorService(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
+
+    public static Professor findProfessorByName(String name) {
+        return professorRepository.findByName(name);
+    }
+
+    public static Professor findProfessorByCode(String code) {
+        return professorRepository.findByName(code);
+    }
+}
