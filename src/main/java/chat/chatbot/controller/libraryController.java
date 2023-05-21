f53ad20b8f2dff4809fa4880b1b4e59d5ab1e2d9
Seokhyun Lee <lseokhyun99@gmail.com>
2023-05-21 23:19:27 +0900

    도서관 좌석 컨트롤러 수정

--- src/main/java/chat/chatbot/controller/libraryController.java
+++ src/main/java/chat/chatbot/controller/libraryController.java
@@ -1,20 +1,19 @@
 package chat.chatbot.controller;
 
+import chat.chatbot.menu.LibrarySeat;
+import chat.chatbot.service.libraryService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class libraryController {
 
     @PostMapping("/libraries")
-    public String librariesData() {
-        // 받은 데이터를 처리하는 로직 작성
-        //System.out.println("Received data: " + data);
-
-        // 응답 데이터를 생성하여 반환
-        String response = "Data received successfully!";
-        return response;
+    public LibrarySeat[] librariesData() throws IOException {
+        return libraryService.getLibrarySeats();
     }
 }
