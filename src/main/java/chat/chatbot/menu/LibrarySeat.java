9bd9f52b9bb6af4ca2fe3b432e04e1a24c3e427c
Seokhyun Lee <lseokhyun99@gmail.com>
2023-05-21 23:17:51 +0900

    도서관 좌석 클래스 추가

---
+++ src/main/java/chat/chatbot/menu/LibrarySeat.java
@@ -0,0 +1,57 @@
+package chat.chatbot.menu;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class LibrarySeat {
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String location;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private Integer all_seats;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private Integer using;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private Integer available;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String operating_hours;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getAll_seats() {
+        return all_seats;
+    }
+
+    public void setAll_seats(Integer all_seats) {
+        this.all_seats = all_seats;
+    }
+
+    public Integer getUsing() {
+        return using;
+    }
+
+    public void setUsing(Integer using) {
+        this.using = using;
+    }
+
+    public Integer getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Integer available) {
+        this.available = available;
+    }
+
+    public String getOperating_hours() {
+        return operating_hours;
+    }
+
+    public void setOperating_hours(String operating_hours) {
+        this.operating_hours = operating_hours;
+    }
+}
