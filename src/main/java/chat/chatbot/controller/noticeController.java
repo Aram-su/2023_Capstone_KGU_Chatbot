ae2f113fa1cdd0cae95076057331977815428f83
Seokhyun Lee <lseokhyun99@gmail.com>
2023-05-22 17:11:11 +0900

    컨트롤러 명 수정 announceController > noticeController NoticeService.getAllNotice 호출, 데이터 반환

---
+++ src/main/java/chat/chatbot/controller/noticeController.java
@@ -0,0 +1,19 @@
+package chat.chatbot.controller;
+
+import chat.chatbot.menu.Notice;
+import chat.chatbot.service.NoticeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class noticeController {
+
+    @PostMapping("/announcements")
+    public List<Notice> announcementsData() {
+        return NoticeService.getAllNotice();
+    }
+}
