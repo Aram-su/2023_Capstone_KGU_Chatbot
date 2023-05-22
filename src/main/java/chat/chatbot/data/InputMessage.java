207c1c8e201027e66b1050e8abd0d25cb423cb86
Seokhyun Lee <lseokhyun99@gmail.com>
2023-05-23 00:23:18 +0900

    채팅창 메시지 데이터 수신을 위한 클래스 InputMessage 클래스 작성

---
+++ src/main/java/chat/chatbot/data/InputMessage.java
@@ -0,0 +1,29 @@
+package chat.chatbot.data;
+
+public class InputMessage {
+    private String type;
+    private String message;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
+
+
+
+
+
+
